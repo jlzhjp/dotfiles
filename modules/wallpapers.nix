9a3ebc064422b05bcac9856a40ce3a903fe232fe
akari <jvjdev@gmail.com>
2026-05-03 22:54:01 +0900

    Sync wallpapers with rclone

--- modules/wallpapers.nix
+++ modules/wallpapers.nix
@@ -3,22 +3,30 @@
 {
   systemd.user.services.rclone-onedrive-wallpapers = {
     Unit = {
-      Description = "Mount OneDrive Wallpapers with rclone";
+      Description = "Sync OneDrive Wallpapers with rclone";
       Documentation = "man:rclone(1)";
       After = [ "network-online.target" ];
       Wants = [ "network-online.target" ];
     };
 
     Service = {
-      Type = "simple";
+      Type = "oneshot";
       ExecCondition = "${pkgs.runtimeShell} -c '${pkgs.rclone}/bin/rclone listremotes | ${pkgs.gnugrep}/bin/grep -Fx onedrive:'";
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/Wallpapers";
-      ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive:Wallpapers %h/Wallpapers --vfs-cache-mode full";
-      ExecStop = "${pkgs.fuse3}/bin/fusermount3 -u %h/Wallpapers";
-      Restart = "on-failure";
-      RestartSec = "10s";
+      ExecStart = "${pkgs.rclone}/bin/rclone sync onedrive:Wallpapers %h/Wallpapers";
     };
 
     Install.WantedBy = [ "default.target" ];
+  };
+
+  systemd.user.timers.rclone-onedrive-wallpapers = {
+    Unit.Description = "Sync OneDrive Wallpapers every 30 minutes";
+
+    Timer = {
+      OnUnitActiveSec = "30min";
+      Unit = "rclone-onedrive-wallpapers.service";
+    };
+
+    Install.WantedBy = [ "timers.target" ];
   };
 }
